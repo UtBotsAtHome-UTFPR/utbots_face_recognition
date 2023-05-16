0f360353168ec1299c606022b9e4eba6b5465e08
DavidSegalle <davidsegalle@alunos.utfpr.edu.br>
2023-05-16 14:06:15 -0300

    Organization and comments

--- src/recognize.py
+++ src/recognize.py
@@ -12,9 +12,6 @@ class FaceRecognizer():
 
     def __init__(self, new_topic_rgbImg):
 
-        # Messages
-        #self.msg_rgbImg = Image()   # Image
-
         # OpenCV
         self.cv_img = None           # CvImage
         self.bridge = CvBridge()
@@ -53,24 +50,23 @@ class FaceRecognizer():
 
     def load_train_data(self):
 
-        
         ''' Model path, turn into a variable '''
         with open('src/face_recognition/trained_knn_model.clf', 'rb') as f:
             self.knn_clf = pickle.load(f)
-
     
     def recognize(self):
 
         self.face_locations = face_recognition.face_locations(self.cv_img)
         self.face_encodings = face_recognition.face_encodings(self.cv_img, self.face_locations)
 
-        # Recognizing bit, move away from here
         if len(self.face_locations) == 0:
             return []
 
+        # Calculates which person is more similar to each face
         closest_distances = self.knn_clf.kneighbors(self.face_encodings, n_neighbors=1)
         are_matches = [closest_distances[0][i][0] <= 0.6 for i in range(len(self.face_locations))]
 
+        # Turn into a ros_msgs and publish the data
         print([(pred, loc) if rec else ("unknown", loc) for pred, loc, rec in zip(self.knn_clf.predict(self.face_encodings), self.face_locations, are_matches)])
 
     # Recognize people from a single photo with the subject's name
@@ -96,22 +92,12 @@ class FaceRecognizer():
             matches = face_recognition.compare_faces(self.known_face_encodings, face_encoding)
             name = "Unknown"
 
-            # # If a match was found in known_face_encodings, just use the first one.
-            # if True in matches:
-            #     first_match_index = matches.index(True)
-            #     name = known_face_names[first_match_index]
-
-            # Or instead, use the known face with the smallest distance to the new face
             face_distances = face_recognition.face_distance(self.known_face_encodings, face_encoding)
             best_match_index = np.argmin(face_distances)
             if matches[best_match_index]:
                 name = self.known_face_names[best_match_index]
 
             face_names.append(name)
-        
-        #self.show_frame(self.face_locations)
-                
-
     
     def mainLoop(self):
         while rospy.is_shutdown() == False:
