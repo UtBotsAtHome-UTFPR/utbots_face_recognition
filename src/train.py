3aa04426efcd1724614931ca5735ef6207ee68fa
DavidSegalle <davidsegalle@alunos.utfpr.edu.br>
2023-05-24 14:24:02 -0300

    Code refactoring

--- src/train.py
+++ src/train.py
@@ -17,10 +17,6 @@ import timeit
 
 class Trainer:
 
-    #train_dir = os.path.realpath(os.path.dirname(__file__)) + "/../faces"
-    #model_save_path = os.path.realpath(os.path.dirname(__file__)) + "/../trained_knn_model.clf"
-
-
     def __init__(self, new_topic_rgbImg):
 
         self.train_dir = os.path.realpath(os.path.dirname(__file__)) + "/../faces"
@@ -45,34 +41,36 @@ class Trainer:
 
         # Algorithm variables
 
+        # Eventually update verbose for making debugging easier (when it uses voice commands and so on)
         self.verbose = True # If set to True prints to the terminal when an image is unfit
+        self.should_face_crop = True # If set to true, crops the images being taken to have only a face on them
+
 
         self.names = []
         self.face_encodings = []
-
         self.n_neighbors = None
 
-        self.picture_taker()
+        # Routine for taking and saving pictures of a subject
+        path = self.picture_path_maker()
+        self.picture_taker(path)
 
         self.start_time = timeit.default_timer()
         
+        # Routine for training, only necessary after all people have been added
         #self.load_faces()
-
         #self.train_data()
 
         self.end_time = timeit.default_timer()
 
         print(str(self.end_time - self.start_time) + " sec.")
 
-        #self.train()
-
     def callback_rgbImg(self, msg):
         self.msg_rgbImg = msg
         self.cv_img = self.bridge.imgmsg_to_cv2(msg, desired_encoding="rgb8")
         self.new_rgbImg = True
 
-    def picture_taker(self):
-        #Take pictures from ros and save them as files
+    def picture_path_maker(self):
+        #Take pictures from ros and save them as files for training
         name = None
         path = None
         while(not name):
@@ -101,6 +99,9 @@ class Trainer:
 
                 else:
                     name = None
+        return path
+    
+    def picture_taker(self, path):
         
         i = 0
         while(i < 15):
@@ -118,22 +119,23 @@ class Trainer:
             time.sleep(4)
 
             # Take in pictures for the new person and save them
+            img = self.cv_img
+            face_bounding_boxes = face_recognition.face_locations(img)
+
             
-            face_bounding_boxes = face_recognition.face_locations(self.cv_img)
-
-            print(face_bounding_boxes[0][0])
-            print(face_bounding_boxes[0][1])
-            print(face_bounding_boxes[0][2])
-            print(face_bounding_boxes[0][3])
-
-            print(face_bounding_boxes)
 
             if len(face_bounding_boxes) == 1:
-                cv2.imwrite(path + "/" + str(i) + '.jpg', cv2.cvtColor(self.cv_img[face_bounding_boxes[0][0]:face_bounding_boxes[0][2], face_bounding_boxes[0][3]:face_bounding_boxes[0][1]], cv2.COLOR_BGR2RGB))
+                if self.should_face_crop:
+                    cv2.imwrite(path + "/" + str(i) + '.jpg', cv2.cvtColor(img[face_bounding_boxes[0][0]:face_bounding_boxes[0][2], face_bounding_boxes[0][3]:face_bounding_boxes[0][1]], cv2.COLOR_BGR2RGB))
+                else:
+                    cv2.imwrite(path + "/" + str(i) + '.jpg', cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+
             else:
                 i -= 1
                 print("Failed, ", end="")
+
             i += 1
+
         print("You're done, congratulations and thank you very much")
     
             
@@ -176,48 +178,6 @@ class Trainer:
         if self.model_save_path is not None:
             with open(self.model_save_path, 'wb') as f:
                 pickle.dump(knn_clf, f)
-        
-
-    '''def __init__(self):
-
-        #rospy.init_node('train_face_recognition', anonymous=True)
-
-        X = []
-        y = []
-
-        # Loop through each person in the training set
-        for class_dir in os.listdir(self.train_dir):
-            if not os.path.isdir(os.path.join(self.train_dir, class_dir)):
-                continue
-
-            # Loop through each training image for the current person
-            for img_path in image_files_in_folder(os.path.join(self.train_dir, class_dir)):
-                image = face_recognition.load_image_file(img_path)
-                face_bounding_boxes = face_recognition.face_locations(image)
-
-                if len(face_bounding_boxes) != 1:
-                    # If there are no people (or too many people) in a training image, skip the image.
-                    if self.verbose:
-                        print("Image {} not suitable for training: {}".format(img_path, "Didn't find a face" if len(face_bounding_boxes) < 1 else "Found more than one face"))
-                else:
-                    # Add face encoding for current image to the training set
-                    X.append(face_recognition.face_encodings(image, known_face_locations=face_bounding_boxes)[0])
-                    y.append(class_dir)
-
-        # Determine how many neighbors to use for weighting in the KNN classifier
-        if self.n_neighbors is None:
-            self.n_neighbors = int(round(math.sqrt(len(X))))
-            if self.verbose:
-                print("Chose n_neighbors automatically:", self.n_neighbors)
-
-        # Create and train the KNN classifier
-        knn_clf = neighbors.KNeighborsClassifier(n_neighbors=self.n_neighbors, algorithm=self.knn_algo, weights='distance')
-        knn_clf.fit(X, y)
-
-        # Save the trained KNN classifier
-        if self.model_save_path is not None:
-            with open(self.model_save_path, 'wb') as f:
-                pickle.dump(knn_clf, f)'''
 
 
 if __name__ == "__main__":
