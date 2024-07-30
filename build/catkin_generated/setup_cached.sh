5a524489da4589eaf07967fadaab90f90af08670
David Segalle <davidsegalle@alunos.utfpr.edu.br>
2024-07-30 14:38:31 -0300

    Merge

--- build/catkin_generated/setup_cached.sh
+++ build/catkin_generated/setup_cached.sh
@@ -8,6 +8,8 @@
 
 # modified environment variables
 export CMAKE_PREFIX_PATH="/home/laser/catkin_ws/src/utbots_face_recognition/build/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/home/laser/catkin_ws/src/utbots_face_recognition/build/devel/lib:$LD_LIBRARY_PATH"
+export PKG_CONFIG_PATH="/home/laser/catkin_ws/src/utbots_face_recognition/build/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
 export PWD='/home/laser/catkin_ws/src/utbots_face_recognition/build'
 export ROSLISP_PACKAGE_DIRECTORIES="/home/laser/catkin_ws/src/utbots_face_recognition/build/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
 export ROS_PACKAGE_PATH="/home/laser/catkin_ws/src/utbots_face_recognition:$ROS_PACKAGE_PATH"
